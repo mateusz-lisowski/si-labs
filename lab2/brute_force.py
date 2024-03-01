3c4f57c7f36e8c60a8e6acbcde5d747a0e7fe828
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-01 13:24:04 +0100

    Adjust scripts to fit pep8

--- lab2/brute_force.py
+++ lab2/brute_force.py
@@ -3,6 +3,7 @@ import time
 
 from data import *
 
+
 def fitness(items, knapsack_max_capacity, solution):
     total_weight = sum(compress(items['Weight'], solution))
     if total_weight > knapsack_max_capacity:
@@ -10,20 +11,24 @@ def fitness(items, knapsack_max_capacity, solution):
     return sum(compress(items['Value'], solution))
 
 
+def main():
+    items, knapsack_max_capacity = get_small()
+    print(items)
 
-items, knapsack_max_capacity = get_small()
-print(items)
+    start_time = time.time()
+    best_solution = None
+    best_value = 0
+    for solution in product([False, True], repeat=len(items)):
+        solution_value = fitness(items, knapsack_max_capacity, solution)
+        if solution_value > best_value:
+            best_solution = solution
+            best_value = solution_value
+    end_time = time.time()
+    total_time = end_time - start_time
+    print('Best solution:', list(compress(items['Name'], best_solution)))
+    print('Best solution value:', best_value)
+    print('Time: ', total_time)
 
-start_time = time.time()
-best_solution = None
-best_value = 0
-for solution in product([False, True], repeat=len(items)):
-    solution_value = fitness(items, knapsack_max_capacity, solution)
-    if solution_value > best_value:
-        best_solution = solution
-        best_value = solution_value
-end_time = time.time()
-total_time = end_time - start_time
-print('Best solution:', list(compress(items['Name'], best_solution)))
-print('Best solution value:', best_value)
-print('Time: ', total_time)
+
+if __name__ == '__main__':
+    main()
--- lab2/data.py
+++ lab2/data.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+
 def get_small():
     knapsack = pd.read_csv('knapsack-small.csv')
     return knapsack, 10
+
 
 def get_big():
     knapsack = pd.read_csv('knapsack-big.csv')
