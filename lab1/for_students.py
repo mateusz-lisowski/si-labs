7f29be7f7dd785afc78918728f0788b0338d4179
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-01 12:10:12 +0100

    Add lab no. 1 for 3 points

--- lab1/for_students.py
+++ lab1/for_students.py
@@ -19,13 +19,37 @@ train_data, test_data = split_data(data)
 y_train = train_data['MPG'].to_numpy()
 x_train = train_data['Weight'].to_numpy()
 
-y_test = test_data['MPG'].to_numpy()
+y_test = test_data['MPG'].to_numpy()    # actual output
 x_test = test_data['Weight'].to_numpy()
 
-# TODO: calculate closed-form solution
-theta_best = [0, 0]
 
-# TODO: calculate error
+def extend_with_zeros(array: np.array) -> np.ndarray:
+    # Reshape the vector to be a column vector
+    zeros_column = array.reshape(-1, 1)
+
+    # Stack the vector_column and ones_column horizontally
+    return np.hstack((np.ones_like(zeros_column), zeros_column))
+
+
+def calculate_mse(t_matrix, x_matrix, y_matrix):
+    return sum([((t_matrix[0] + t_matrix[1] * x) - y) ** 2 for x, y in zip(x_matrix, y_matrix)]) / len(x_matrix)
+
+
+x_train_extended = extend_with_zeros(x_train)
+
+# Compute X^T X
+XtX = np.dot(x_train_extended.T, x_train_extended)
+
+# Compute the inverse of X^T X
+XtX_inv = np.linalg.inv(XtX)
+
+# Compute (X^T X)^{-1} X^T
+XtX_inv_Xt = np.dot(XtX_inv, x_train_extended.T)
+
+# Compute theta
+theta_best = np.dot(XtX_inv_Xt, y_train)    # predicted output
+
+print(f"calculated MSE: {calculate_mse(theta_best, x_test, y_test)}")
 
 # plot the regression line
 x = np.linspace(min(x_test), max(x_test), 100)
@@ -36,17 +60,35 @@ plt.xlabel('Weight')
 plt.ylabel('MPG')
 plt.show()
 
-# TODO: standardization
+x_test_normalized = (x_test - np.average(x_test)) / np.std(x_test)
+y_test_normalized = (y_test - np.average(y_test)) / np.std(y_test)
 
-# TODO: calculate theta using Batch Gradient Descent
+x_train_normalized = (x_train - np.average(x_train)) / np.std(x_train)
+y_train_normalized = (y_train - np.average(y_train)) / np.std(y_train)
 
-# TODO: calculate error
+x_train_extended_normalized = extend_with_zeros(x_train_normalized)
+
+# Define your initial guess for theta
+theta = [0, 0]
+for i in range(100):
+
+    f = 2 / len(x_train_extended_normalized)
+    c1 = np.matmul(x_train_extended_normalized, theta)
+    c2 = np.subtract(c1, y_train_normalized)
+    c3 = np.matmul(x_train_extended_normalized.T, c2)
+    gradient = f * c3
+
+    theta = theta - 0.1 * gradient
+
+theta_best = theta
+
+print(f"gradient MSE: {calculate_mse(theta_best, x_test_normalized, y_test_normalized)}")
 
 # plot the regression line
-x = np.linspace(min(x_test), max(x_test), 100)
+x = np.linspace(min(x_test_normalized), max(x_test_normalized), 100)
 y = float(theta_best[0]) + float(theta_best[1]) * x
 plt.plot(x, y)
-plt.scatter(x_test, y_test)
+plt.scatter(x_test_normalized, y_test_normalized)
 plt.xlabel('Weight')
 plt.ylabel('MPG')
 plt.show()
