feb414122c23d05959d05d2bcb36ff2d17f9b83a
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-03-05 14:11:35 +0100

    Adjust code to fit pep8

--- lab5/k_means.py
+++ lab5/k_means.py
@@ -1,34 +1,39 @@
 import numpy as np
 
+
 def initialize_centroids_forgy(data, k):
     # TODO implement random initialization
     return None
 
+
 def initialize_centroids_kmeans_pp(data, k):
-    # TODO implement kmeans++ initizalization
+    # TODO implement kmeans++ initialization
     return None
+
 
 def assign_to_cluster(data, centroid):
     # TODO find the closest cluster for each data point
     return None
 
+
 def update_centroids(data, assignments):
     # TODO find new centroids based on the assignments
     return None
 
+
 def mean_intra_distance(data, assignments, centroids):
     return np.sqrt(np.sum((data - centroids[assignments, :])**2))
 
-def k_means(data, num_centroids, kmeansplusplus= False):
-    # centroids initizalization
-    if kmeansplusplus:
+
+def k_means(data, num_centroids, kmeans_plus_plus=False):
+    # centroids initialization
+    if kmeans_plus_plus:
         centroids = initialize_centroids_kmeans_pp(data, num_centroids)
     else: 
         centroids = initialize_centroids_forgy(data, num_centroids)
-
     
-    assignments  = assign_to_cluster(data, centroids)
-    for i in range(100): # max number of iteration = 100
+    assignments = assign_to_cluster(data, centroids)
+    for i in range(100):    # max number of iteration = 100
         print(f"Intra distance after {i} iterations: {mean_intra_distance(data, assignments, centroids)}")
         centroids = update_centroids(data, assignments)
         new_assignments = assign_to_cluster(data, centroids)
