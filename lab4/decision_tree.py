f29afd39e478e55a3eaba2f367d1acf2cab1e5bd
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-04-10 22:24:27 +0200

    Remove unused functions

--- lab4/decision_tree.py
+++ lab4/decision_tree.py
@@ -36,7 +36,7 @@ class Node:
         self.value = value
 
 
-class DecisionTreeClassifier:
+class DecisionTree:
     def __init__(self, min_samples_split=2, max_depth=2):
 
         # initialize the root of the tree
@@ -46,7 +46,7 @@ class DecisionTreeClassifier:
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
 
-    def build_tree(self, dataset, curr_depth=0):
+    def train(self, dataset, curr_depth=0):
 
         X, Y = dataset[:, :-1], dataset[:, -1]
         num_samples, num_features = np.shape(X)
@@ -58,9 +58,9 @@ class DecisionTreeClassifier:
             # check if information gain is positive
             if best_split["info_gain"] > 0:
                 # recur left
-                left_subtree = self.build_tree(best_split["dataset_left"], curr_depth + 1)
+                left_subtree = self.train(best_split["dataset_left"], curr_depth + 1)
                 # recur right
-                right_subtree = self.build_tree(best_split["dataset_right"], curr_depth + 1)
+                right_subtree = self.train(best_split["dataset_right"], curr_depth + 1)
                 # return decision node
                 return Node(best_split["feature_index"], best_split["threshold"],
                             left_subtree, right_subtree, best_split["info_gain"])
@@ -88,7 +88,7 @@ class DecisionTreeClassifier:
                 if len(dataset_left) > 0 and len(dataset_right) > 0:
                     y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
                     # compute information gain
-                    curr_info_gain = self.information_gain(y, left_y, right_y, "gini")
+                    curr_info_gain = self.information_gain(y, left_y, right_y)
                     # update the best split if needed
                     if curr_info_gain > max_info_gain:
                         best_split["feature_index"] = feature_index
@@ -102,32 +102,16 @@ class DecisionTreeClassifier:
         return best_split
 
     def split(self, dataset, feature_index, threshold):
-
         dataset_left = np.array([row for row in dataset if row[feature_index] <= threshold])
         dataset_right = np.array([row for row in dataset if row[feature_index] > threshold])
         return dataset_left, dataset_right
 
-    def information_gain(self, parent, l_child, r_child, mode="entropy"):
-
+    def information_gain(self, parent, l_child, r_child):
         weight_l = len(l_child) / len(parent)
         weight_r = len(r_child) / len(parent)
-        if mode == "gini":
-            gain = self.gini_index(parent) - (weight_l * self.gini_index(l_child) + weight_r * self.gini_index(r_child))
-        else:
-            gain = self.entropy(parent) - (weight_l * self.entropy(l_child) + weight_r * self.entropy(r_child))
-        return gain
-
-    def entropy(self, y):
-
-        class_labels = np.unique(y)
-        entropy = 0
-        for cls in class_labels:
-            p_cls = len(y[y == cls]) / len(y)
-            entropy += -p_cls * np.log2(p_cls)
-        return entropy
+        return self.gini_index(parent) - (weight_l * self.gini_index(l_child) + weight_r * self.gini_index(r_child))
 
     def gini_index(self, y):
-
         class_labels = np.unique(y)
         gini = 0
         for cls in class_labels:
@@ -157,7 +141,7 @@ class DecisionTreeClassifier:
 
     def fit(self, X, Y):
         dataset = np.concatenate((X, Y), axis=1)
-        self.root = self.build_tree(dataset)
+        self.root = self.train(dataset)
 
     def predict(self, X):
         preditions = [self.make_prediction(x, self.root) for x in X]
