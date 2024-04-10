f7ad27105eab391a182abc16034657e9447a2f2a
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-04-11 00:11:13 +0200

    Implement working random forest classifier

--- lab4/main.py
+++ lab4/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from decision_tree import DecisionTree
-# from random_forest_solution import RandomForest
+from random_forest import RandomForest
 from load_data import generate_data, load_titanic
 
 
@@ -15,10 +15,10 @@ def main():
     dt.evaluate(*train_data)
     dt.evaluate(*test_data)
 
-    # rf = RandomForest({"ntrees": 10, "feature_subset": 2, "depth": 14})
-    # rf.train(*train_data)
-    # rf.evaluate(*train_data)
-    # rf.evaluate(*test_data)
+    rf = RandomForest({"ntrees": 10, "feature_subset": 2, "depth": 14})
+    rf.train(*train_data)
+    rf.evaluate(*train_data)
+    rf.evaluate(*test_data)
 
 
 if __name__ == '__main__':
